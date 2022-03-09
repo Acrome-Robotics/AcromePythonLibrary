48fd36d251d42639587f9ea414eedf646a4cc1aa
Umut Ediz <umut@ediz.dev>
2022-03-09 14:29:09 +0300

    Add OneDOF class

--- src/acrome/controller.py
+++ src/acrome/controller.py
@@ -19,3 +19,48 @@ class Controller():
 
     def _crc32(self, data):
         return CRC32.calc(data).to_bytes(4, 'little')
+
+class OneDOF(Controller):
+    _DEVID = 0xBA
+    _EN_MASK = 1 << 0
+    _ENC1_RST_MASK = 1 << 1
+    _ENC2_RST_MASK = 1 << 2
+    _RECEIVE_COUNT = 16
+
+    def __init__(self, portname="/dev/serial0"):
+        super().__init__(portname=portname)
+        self.config = 0
+        self.speed = 0
+        self.angle = 0
+        self.motor_enc = 0
+        self.shaft_enc = 0
+        self.imu = [0,0,0]
+
+    def set_speed(self, speed):
+        if speed != 0:
+            self.speed = speed if abs(speed) <= 1000 else 1000 * (speed / abs(speed))
+
+    def enable(self, en):
+        self.config = (self.config & ~self.__class__._EN_MASK) | (en & self.__class__._EN_MASK)
+
+    def reset_encoder_mt(self):
+        self.config |= self.__class__._ENC1_RST_MASK
+
+    def reset_encoder_shaft(self):
+        self.config |= self.__class__._ENC2_RST_MASK
+    
+    def write(self):
+        data = struct.pack("<BBBh", self.__class__._HEADER, self.__class__._DEVID, self.config, self.speed)
+        data += self._crc32(data)
+        super()._write(data)
+        self.config &= self.__class__._EN_MASK
+
+    def read(self):
+        data = super()._read(self.__class__._RECEIVE_COUNT)
+        if data is not None:
+            if data[self.__class__._ID_INDEX] == self.__class__._DEVID:
+                self.motor_enc = struct.unpack("<H", data[2:4])
+                self.shaft_enc = struct.unpack("<H", data[4:6])
+                self.imu[0] = struct.unpack("<h", data[6:8])
+                self.imu[0] = struct.unpack("<h", data[8:10])
+                self.imu[0] = struct.unpack("<h", data[10:12])
