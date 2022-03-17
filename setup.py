b22416a0ed8878eac279bf4a2b98270115c20a06
Umut Ediz <umut@ediz.dev>
2022-03-17 14:51:18 +0300

    Fix: Package name on PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="acrome-acromerobotics",
+	name="acrome",
 	version="0.0.1",
 	author="Umut Ediz",
 	author_email="umutediz@acrome.net",
