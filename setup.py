812b98b20eeffc77967d4d7e0741ab0756b14da8
Umut Ediz <umut@ediz.dev>
2022-04-29 12:34:37 +0300

    Increment version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name="acrome",
-	version="0.1.2",
+	version="0.2.0",
 	author="Umut Ediz",
 	author_email="umutediz@acrome.net",
 	description="Hardware abstraction library for Acrome Robotics products",
