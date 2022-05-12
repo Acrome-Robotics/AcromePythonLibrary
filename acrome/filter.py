899de353773aed817b99a66b7ca566a27b6d028c
Umut Ediz <umut@ediz.dev>
2022-05-12 12:44:18 +0300

    Add moving average filter class

---
+++ acrome/filter.py
@@ -0,0 +1,12 @@
+class MovingAverage():
+    def __init__(self, size):
+        self.__array = [0]*size
+        self.__iter = 0
+        self.__output = 0
+    
+    def apply(self, val):
+        out = self.__output - self.__array[self.__iter]/len(self.__array)
+        self.__array[self.__iter] = float(val)
+        self.__output = out + self.__array[self.__iter]/len(self.__array)
+        self.__iter = (self.__iter + 1) % len(self.__array)
+        return self.__output
