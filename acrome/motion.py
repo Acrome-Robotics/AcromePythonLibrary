630843e0dac7d0b159beeee4f27a458fe3bab3a8
Umut Ediz <umut@ediz.dev>
2022-06-21 15:25:09 +0300

    Fix: control object init

--- acrome/motion.py
+++ acrome/motion.py
@@ -7,7 +7,7 @@ import numpy as np
 class AutoOneDOF(controller.OneDOF):
     def __init__(self, interval=0.004, *args, **kwargs):
         self.__interval = interval
-        self.control = [autocontrol.PID()] * 1
+        self.control = [autocontrol.PID()]
         self.control[0].set_gains({'ff':750, 'kp':0.9, 'kd':0.1, 'ki':0.01, 'antiwindup':30, 'deadband':(0,0)})
         for ctrl in self.control:
             ctrl.set_interval(self.__interval)
@@ -28,7 +28,7 @@ class AutoOneDOF(controller.OneDOF):
 class AutoBallBeam(controller.BallBeam):
     def __init__(self, interval = 0.004, *args, **kwargs):
         self.__interval = interval
-        self.control = [autocontrol.PID()] * 1
+        self.control = [autocontrol.PID()]
         self.control[0].set_gains({'ff':0, 'kp':0.35, 'kd':0.15, 'ki':0.1, 'antiwindup':30, 'deadband':(0,0)})
         self.control[0].config_filter(20, 'error')
         self.control[0].config_filter(20, 'derivative')
@@ -51,7 +51,7 @@ class AutoBallBeam(controller.BallBeam):
 class AutoBallBalancingTable(controller.BallBalancingTable):
     def __init__(self, interval=0.004, *args, **kwargs):
         self.__interval = interval
-        self.control = [autocontrol.PID()] * 2
+        self.control = [autocontrol.PID(), autocontrol.PID()]
 
         self.control[0].set_gains({'ff':0, 'kp':1.7, 'kd':0.5, 'ki':0.5, 'antiwindup':30, 'deadband':(0,0)})
         self.control[0].config_filter(20, 'derivative')
@@ -80,7 +80,7 @@ class AutoBallBalancingTable(controller.BallBalancingTable):
 class AutoDelta(controller.Delta):
     def __init__(self, interval=1, *args, **kwargs):
         self.__interval = interval
-        self.control = [autocontrol.PID()] * 3
+        self.control = [autocontrol.PID() for _ in range(3)]
         
         for control in self.control:
             control.set_gains({'ff':0, 'kp':1.0, 'kd':0.00, 'ki':0, 'antiwindup':0, 'deadband':(0,0)})
@@ -109,7 +109,7 @@ class AutoDelta(controller.Delta):
 class AutoStewart(controller.Stewart):
     def __init__(self, interval=1, *args, **kwargs):
         self.__interval = interval
-        self.control = [autocontrol.PID()] * 6
+        self.control = [autocontrol.PID() for _ in range(6)]
         for control in self.control:
             control.config_filter(20)
         self.__pos_min = [0] * 6
